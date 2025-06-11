2ddb78308454834ac25682b640e0ccecc0126566
Yogesh2061 <ybhandari461@gmail.com>
2025-06-11 23:58:13 +0545

    js

---
+++ Concatenation and templete literals .js
@@ -0,0 +1,12 @@
+// Concatenation 
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName +" is 22 years old.";
+console.log(fullName); // Output: John Doe
+
+//string concatenation can become cumbersome and less readable, especially with longer strings and multiple variables.
+
+// Template Literals
+let age = 22;
+let message = `my name is ${firstName} and I am ${age} years old.`;
+console.log(message); // Output: my name is John and I am 22 years old.
